9991115355df10d3d897fc204ad3513ab084a29b
Wh1isper <9573586@qq.com>
2023-03-25 02:06:17 +0800

    Fix bugs on case with subcases

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-from typing import List
 
 import pytest
 
@@ -9,28 +8,28 @@ from km2testcase.parser import parse_km
 
 _here = os.path.dirname(os.path.abspath(__file__))
 
-cases = [
-    (
-        os.path.join(_here, "./case1.km"),
-        [
-            CaseModel(
-                project_name="产品名称",
-                model_name="模块1",
-                case_name="测试用例-子用例1",
-                priority=2,
-                prepare=f"\n{PREPARE_SEP}\n".join(["前置步骤", "前置步骤2"]),
-                steps=[Step(step="步骤1", expect="期望1")],
-            ),
-            CaseModel(
-                project_name="产品名称",
-                model_name="模块1",
-                case_name="测试用例",
-                priority=2,
-                prepare="前置步骤",
-                steps=[Step(step="步骤2", expect="期望2")],
-            ),
-        ],
+CASE_MODEL_1 = [
+    CaseModel(
+        project_name="产品名称",
+        model_name="模块1",
+        case_name="测试用例-子用例1",
+        priority=2,
+        prepare=f"\n{PREPARE_SEP}\n".join(["前置步骤", "前置步骤2"]),
+        steps=[Step(step="步骤1", expect="期望1")],
     ),
+    CaseModel(
+        project_name="产品名称",
+        model_name="模块1",
+        case_name="测试用例",
+        priority=2,
+        prepare="前置步骤",
+        steps=[Step(step="步骤2", expect="期望2")],
+    ),
+]
+
+cases = [
+    (os.path.join(_here, "./case1.km"), CASE_MODEL_1),
+    (os.path.join(_here, "./case2.km"), CASE_MODEL_1),
     (
         os.path.join(_here, "../EXAMPLE.km"),
         [
@@ -85,17 +84,15 @@ cases = [
 ]
 
 
-def assert_node(case_models: List[CaseModel], expected_models: List[CaseModel]):
-    assert sorted(case_models) == sorted(expected_models)
-
-
 @pytest.mark.parametrize("case", cases)
 def test_convert_case_model(
     case,
 ):
-    markdown_file_path, result = case
+    markdown_file_path, expects = case
     case_models = parse_km(Path(markdown_file_path))
-    assert case_models == result
+    assert len(case_models) == len(expects)
+    for case_model in case_models:
+        assert case_model in expects
 
 
 if __name__ == "__main__":
